ab90c54bacce678a6e608904e891357a7e00acc4
Nikita <nikitazapletin@gmail.com>
2022-12-07 18:46:26 +0300

    correction

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,34 +1,52 @@
 const modals = () => {
+   const modalTimeout = showModalByTime(".popup", 3000);
+   
    function bindModal(triggerSelector, modalSelector, closeSelector) {
-      const trigger = document.querySelectorAll(triggerSelector),
-         modal = document.querySelector(modalSelector),
-         close = document.querySelector(closeSelector);
+      const triggers = document.querySelectorAll(triggerSelector);
+      const modal = document.querySelector(modalSelector);
+      const close = document.querySelector(closeSelector);
 
-      trigger.forEach((item) => {
-         item.addEventListener("click", (e) => {
+      triggers.forEach((trigger) => {
+         trigger.addEventListener("click", (e) => {
             if (e.target) {
                e.preventDefault();
-               modal.style.display = "block";
-               document.body.style.overflow = "hidden";
+               showModal(modal);
             }
          });
       });
 
       close.addEventListener("click", () => {
-         modal.style.display = "none";
-         document.body.style.overflow = "";
+         hideModal(modal);
       });
 
       modal.addEventListener("click", (e) => {
          if (e.target === modal) {
-            modal.style.display = "none";
-            document.body.style.overflow = "";
+            hideModal(modal);
          }
       });
    }
 
+   function showModal(modal) {
+      clearTimeout(modalTimeout);
+      modal.style.display = "block";
+      document.addEventListener(
+         "keydown",
+         (e) => {
+            if (e.key === "Escape") {
+               hideModal(modal);
+            }
+         },
+         { once: true }
+      );
+   }
+
+   function hideModal(modal) {
+      modal.style.display = "none";
+      document.body.style.overflow = "";
+   }
+
    function showModalByTime(selector, time) {
-      setTimeout(() => {
+      return setTimeout(() => {
          document.querySelector(selector).style.display = "block";
          document.body.style.overflow = "hidden";
       }, time);
@@ -40,7 +58,6 @@ const modals = () => {
       ".popup_engineer .popup_close"
    );
    bindModal(".phone_link", ".popup", ".popup .popup_close");
-   showModalByTime(".popup", 3000);
 };
 
 export default modals;
